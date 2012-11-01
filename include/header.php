962c164e5b3adcd175f1bf02fcb650dca337dd64
Jeroen Seegers <jeroen.seegers+github@gmail.com>
2012-11-01 22:01:32 +0100

    Split up the UI in different files for easier maintainability.

---
+++ include/header.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>OpenArena Statistics</title>
+        <link href="/css/bootstrap.css" rel="stylesheet">
+        <link href="/css/style.css" rel="stylesheet">
+        <style type="text/css">
+            body {
+                padding-top: 60px;
+                padding-bottom: 40px;
+            }
+            .sidebar-nav {
+                padding: 9px 0;
+            }
+        </style>
+        <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+        <!--[if lt IE 9]>
+        <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+    </head>
+
+    <body>
+
+        <div class="navbar navbar-fixed-top">
+            <div class="navbar-inner">
+                <div class="container-fluid">
+                    <a class="brand" href="#">OpenArena Statistics</a>
+                    <ul class="nav" role="navigation">
+                        <li class="dropdown">
+                            <a id="players" href="#" role="button" class="dropdown-toggle" data-toggle="dropdown">Players <b class="caret"></b></a>
+                            <ul class="dropdown-menu" role="menu" aria-labelledby="players">
+                                <?php if (!isset($_GET['player'])) : ?>
+                                <li class="active"><a href="/">Overall</a></li>
+                                <?php else : ?>
+                                <li><a href="/">Overall</a></li>
+                                <?php endif; ?>
+                                <li class="divider"></li>
+                                <?php while ($player_row = mysqli_fetch_array($players_result, MYSQLI_ASSOC)) : ?>
+                                    <?php if (isset($_GET['player']) && $player_row['id'] == $_GET['player']) : ?>
+                                        <li class="active"><a href="?player=<?php echo $player_row['id']; ?>"><?php echo $player_row['nickname']; ?></a></li>
+                                    <?php else : ?>
+                                        <li><a href="?player=<?php echo $player_row['id']; ?>"><?php echo $player_row['nickname']; ?></a></li>
+                                    <?php endif; ?>
+                                <?php endwhile; ?>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
---
+++ include/sidebar.php
@@ -0,0 +1,28 @@
+                    <!--
+                    <form class="well form-horizontal">
+                        <div class="control-group">
+                            <label class="control-label" for="from-date">From:</label>
+                            <div class="controls">
+                                <select id="from-date" name="from-date">
+                                    <option value="alltime">Alltime</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="control-group">
+                            <label class="control-label" for="until-date">Until:</label>
+                            <div class="controls">
+                                <select id="until-date" name="until-date">
+                                    <option value="alltime">Alltime</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="control-group">
+                            <input type="submit" class="btn btn-primary" value="Submit">
+                        </div>
+                    </form>
+                    -->
+                    <div class="well sidebar-nav">
+                        <ul class="nav nav-list">
+                            <li class="nav-header">Filters</li>
+                        </ul>
+                    </div>
