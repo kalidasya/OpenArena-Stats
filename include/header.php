43a0082b05c9420e670b947c53495d4ed46185e2
kalidasya <oliolart@gmail.com>
2013-12-09 20:14:44 +0100

    add basic graph

--- include/header.php
+++ include/header.php
@@ -18,6 +18,7 @@
         <!--[if lt IE 9]>
         <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
         <![endif]-->
+        <script type="text/javascript" src="https://www.google.com/jsapi"></script>
     </head>
 
     <body>
--- include/overall.php
+++ include/overall.php
@@ -198,6 +198,44 @@
                             </table>
                         </div>
                     </div>
+                    <?php if (isset($_GET['player']) && intval($_GET['player']) > 0) :?>
+                    <div class="row-fluid">
+                    	<div class="span6">
+                    		<table class="table table-striped">
+                                <thead>
+                                    <tr>
+                                        <th>Stats</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                	<tr><td>
+ 									<script>
+	                                    google.load("visualization", "1", {packages:["corechart"]});
+	                                    google.setOnLoadCallback(drawChart);
+
+	                                    function drawChart() {
+	                                      var data = new google.visualization.DataTable();
+										  data.addColumn('date', 'Date');
+										  data.addColumn('number', 'Kills');
+										  data.addColumn('number', 'Deaths');
+	                                      var options = {
+	                                        title: 'User stats'
+	                                      };
+	                                      <?php foreach ($general_stats_rows as $stats)
+	                                      {
+	                                      	echo "data.addRow([new Date(".$stats['year'].",".$stats['month'].",".$stats['day']."), ".$stats['kills'].", ".$stats['deaths']."]);";
+										  }?>	
+	                                      var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+	                                      chart.draw(data, options);
+	                                    }
+                                    </script>
+                                    <div id="chart_div" style="width: 900px; height: 500px;"></div>
+                                    </td></tr>
+                                </tbody>
+                            </table>
+                    	</div>
+                    </div>
+                    <?php endif;?>
                 </div>
             </div>
         </div>
